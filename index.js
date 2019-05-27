5a3a420757ae3f20e856aa189859f1ec369160ef
jangot <Pavel.Pulin@infobip.com>
2019-05-27 08:48:37 +0300

    add run command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var spin_1 = require("./src/class/spin");
+// import { Jump } from './src/class/jump';
 var el = new spin_1.Spin(spin_1.Spin.TYPES[1]);
 el.setLevel(spin_1.Spin.LEVELS[1]);
 console.log(el.getValue());
