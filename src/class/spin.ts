62de508e4f1782e11adcb51d64b0a1c1e405fd04
jangot <Pavel.Pulin@infobip.com>
2019-06-23 20:34:15 +0300

    fix hasError

--- src/class/spin.ts
+++ src/class/spin.ts
@@ -64,7 +64,7 @@ export class Spin extends AbstractFCElement {
     }
 
     hasError() {
-        return Spin.HAS_ERROR.indexOf(this.type);
+        return Spin.HAS_ERROR.indexOf(this.type) > -1;
     }
 
     getKey(): string {
